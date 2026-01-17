f9625ab56e02d07da0f383749549fd1d2a43676f
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-01-17 22:26:33 +0300

    Initial commit

---
+++ sendmailfinal.py
@@ -0,0 +1,44 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+email_from = '0rcb0n3@rambler.ru'
+email_to = '0rcb0n3@rambler.ru'
+password = os.getenv('TOKEN')
+
+weblink = "https://dvmn.org/profession-ref-program/"
+friend_name = "Friend"
+company_name = "Devman"
+
+letter = """\
+From: 0rcb0n3@rambler.ru
+To: 0rcb0n3@rambler.ru
+Subject: Приглашение!
+Content-Type: text/plain; charset="UTF-8";\n
+
+Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
+
+%website% — это новая версия онлайн-курса по программированию. 
+Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
+
+Как будет проходить ваше обучение на %website%? 
+
+→ Попрактикуешься на реальных кейсах. 
+Задачи от тимлидов со стажем от 10 лет в программировании.
+→ Будешь учиться без стресса и бессонных ночей. 
+Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
+→ Подготовишь крепкое резюме.
+Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
+
+Регистрируйся → %website%  
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.
+""".replace('%friend_name%',friend_name).replace('%my_name%',company_name).replace('%website%',weblink)
+
+letter = letter.encode("UTF-8")
+
+server = smtplib.SMTP_SSL('smtp.rambler.ru', 465)
+server.login(email_from,password)
+server.sendmail(email_from, email_to, letter)
+
